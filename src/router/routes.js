6cce3337a4f9b8e356bcf34ea97fbeb7ede20a1b
hsbtr <927390894@qq.com>
2020-11-29 22:29:22 +0800

    更新路由懒加载

--- src/router/routes.js
+++ src/router/routes.js
@@ -20,16 +20,16 @@ export default [
   {
     path: "/Find",
     name: "Find",
-    component: () => import("../views/Find")
+    component: () => import("../views/Find.vue")
   },
   {
     path: "/Mess",
     name: "Mess",
-    component: () => import("../views/Mess")
+    component: () => import("../views/Mess.vue")
   },
   {
     path: "/My",
     name: "My",
-    component: () => import("../views/My")
+    component: () => import("../views/My.vue")
   }
 ];
