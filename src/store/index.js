1a7f153f9efe8a1b044f3e4ae653f5345e068cf3
hsbtr <927390894@qq.com>
2021-01-13 18:15:38 +0800

    增加vuex模块化

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+const files = require.context("./modules", false, /\.js$/);
+const modules = {};
+
+files.keys().forEach(key => {
+  modules[key.replace(/(.*\/)*([^.]+).*/gi, "$2")] = files(key).default;
+});
+console.log(modules);
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
   actions: {},
-  modules: {}
+  modules: modules
 });
---
+++ src/store/modules/user.js
@@ -0,0 +1,21 @@
+import { UPDATE } from "../types";
+
+const state = {
+  person: 1
+};
+
+const getters = {};
+
+const mutations = {
+  [UPDATE]() {}
+};
+
+const actions = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+};
